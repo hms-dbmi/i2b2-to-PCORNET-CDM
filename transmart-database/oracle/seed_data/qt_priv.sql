3bacc2b3bc77a29d1edfcbbe280a26a4c743490b
mtmcduffie <mcduffie.michael@gmail.com>
2015-01-15 14:45:40 -0500

    AVL-4 - Removing leftover cruft from regex.

--- transmart-database/oracle/seed_data/qt_priv.sql
+++ transmart-database/oracle/seed_data/qt_priv.sql
@@ -1,7 +1,6 @@
 --------------------------------------------------------
 --  File created - Wednesday-December-10-2014   
 --------------------------------------------------------
- I2B2DEMODATA.QT_PRIVILEGE
 
 Insert into I2B2DEMODATA.QT_PRIVILEGE (PROTECTION_LABEL_CD,DATAPROT_CD,HIVEMGMT_CD,PLUGIN_ID) values ('PDO_WITHOUT_BLOB','DATA_LDS','USER',null);
 Insert into I2B2DEMODATA.QT_PRIVILEGE (PROTECTION_LABEL_CD,DATAPROT_CD,HIVEMGMT_CD,PLUGIN_ID) values ('PDO_WITH_BLOB','DATA_DEID','USER',null);
--- transmart-database/oracle/seed_data/qt_query_states_type.sql
+++ transmart-database/oracle/seed_data/qt_query_states_type.sql
@@ -1,7 +1,6 @@
 --------------------------------------------------------
 --  File created - Wednesday-December-10-2014   
 --------------------------------------------------------
- I2B2DEMODATA.QT_QUERY_STATUS_TYPE
 
 Insert into I2B2DEMODATA.QT_QUERY_STATUS_TYPE (STATUS_TYPE_ID,NAME,DESCRIPTION) values (1,'QUEUED',' WAITING IN QUEUE TO START PROCESS');
 Insert into I2B2DEMODATA.QT_QUERY_STATUS_TYPE (STATUS_TYPE_ID,NAME,DESCRIPTION) values (2,'PROCESSING','PROCESSING');
