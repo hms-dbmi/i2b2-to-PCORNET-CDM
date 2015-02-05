71f20a8a261eab652f3fb344500589bcc1511ab3
Sushma <sushmapatil123@gmail.com>
2015-02-04 21:18:23 -0500

    Adding Triggers to schema tm_cz

---
+++ transmart-database/oracle/schemas/tm_cz/PROCEDURES/JOB_ID.sql
@@ -0,0 +1,13 @@
+ CREATE OR REPLACE TRIGGER "TM_CZ"."TRG_CZ_JOB_ID"
+  before insert on tm_cz.CZ_JOB_MASTER    for each row
+  begin
+    if inserting then
+      if :NEW.JOB_ID is null then
+        select SEQ_CZ_JOB_MASTER.nextval into :NEW.JOB_ID from dual;
+      end if;
+    end if;
+  end;
+
+
+/
+ALTER TRIGGER "TM_CZ"."TRG_CZ_JOB_ID" ENABLE;
