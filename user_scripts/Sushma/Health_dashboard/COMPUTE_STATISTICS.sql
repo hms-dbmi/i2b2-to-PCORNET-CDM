a25667781c95f66eb80b6d3d3c4b48765fdbd949
Sushma <sushmapatil123@gmail.com>
2015-02-17 17:28:59 -0500

    changing DATASOURCE TO SOURCESYSTEM_CD

--- user_scripts/Sushma/Health_dashboard/COMPUTE_STATISTICS.sql
+++ user_scripts/Sushma/Health_dashboard/COMPUTE_STATISTICS.sql
@@ -1,29 +1,29 @@
 create or replace PROCEDURE COMPUTE_STATISTICS AS 
 BEGIN
 execute immediate('truncate table "BIOMART_USER"."COMPUTE_STATS"');
-commit;
+
 insert into biomart_user.compute_stats
 select i.c_fullname as C_FULLNAME, count(distinct o.patient_num) AS DISTINCT_PATIENTS, count(distinct o.concept_cd) AS DISTINCT_CONCEPTS, count(o.concept_cd) AS NUMBER_OF_FACTS
 from i2b2metadata.i2b2 i, observation_fact o, concept_dimension c
 where i.c_hlevel=1 and o.concept_cd=c.concept_cd and  c.concept_path like concat(i.C_FULLNAME,'%')
  and i.c_fullname not like '\Public Studies\%'
 group by i.c_fullname;
-commit;
+
 
 execute immediate('truncate table biomart_user.concept_stats');
-commit;
+
 insert into biomart_user.concept_stats
 SELECT SOURCESYSTEM_CD, count(1) as COUNT_OF_CONCEPTS FROM CONCEPT_DIMENSION  where concept_path not like '\\Public Studies\\%' group by SOURCESYSTEM_CD;
-commit;
+
 
 execute immediate('truncate table biomart_user.observation_stats');
-commit;
+
 insert into biomart_user.observation_stats
-SELECT DATASOURCE, count(1) as nb_observation_fact, count(distinct patient_num) as distinct_patient_num, count(distinct CONCEPT_CD) as distint_CONCEPT_CD, min(START_DATE) as min_start_date, max(START_DATE) as max_start_date FROM OBSERVATION_FACT where datasource not like  'EGP%' and datasource not like  'GSE%' group by DATASOURCE;
-commit;
+SELECT SOURCESYSTEM_CD, count(1) as nb_observation_fact, count(distinct patient_num) as distinct_patient_num, count(distinct CONCEPT_CD) as distint_CONCEPT_CD, min(START_DATE) as min_start_date, max(START_DATE) as max_start_date FROM OBSERVATION_FACT where SOURCESYSTEM_CD not like  'EGP%' and SOURCESYSTEM_CD not like  'GSE%' group by SOURCESYSTEM_CD;
+
 
 execute immediate('truncate table "BIOMART_USER"."SCHEMA_STATS" ');
-commit;
+
 insert into BIOMART_USER.SCHEMA_STATS 
 SELECT
    owner, to_char(sum(bytes)/1024/1024/1024, '9999.999') as GB
