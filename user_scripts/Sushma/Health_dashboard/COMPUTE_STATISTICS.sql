d971abff327a95704fbb857b2be97a9db1cb8848
Sushma <sushmapatil123@gmail.com>
2015-02-19 14:25:22 -0500

    Grant BIOMART user permissions to SELECT on schema i2b2demodata nad i2b2metadata

--- user_scripts/Sushma/Health_dashboard/COMPUTE_STATISTICS.sql
+++ user_scripts/Sushma/Health_dashboard/COMPUTE_STATISTICS.sql
@@ -1,30 +1,30 @@
 create or replace PROCEDURE COMPUTE_STATISTICS AS 
 BEGIN
-execute immediate('truncate table "BIOMART_USER"."COMPUTE_STATS"');
+execute immediate('truncate table "BIOMART"."COMPUTE_STATS"');
 
-insert into biomart_user.compute_stats
+insert into BIOMART.compute_stats
 select i.c_fullname as C_FULLNAME, count(distinct o.patient_num) AS DISTINCT_PATIENTS, count(distinct o.concept_cd) AS DISTINCT_CONCEPTS, count(o.concept_cd) AS NUMBER_OF_FACTS
-from i2b2metadata.i2b2 i, observation_fact o, concept_dimension c
+from i2b2metadata.i2b2 i, i2b2demodata.observation_fact o, concept_dimension c
 where i.c_hlevel=1 and o.concept_cd=c.concept_cd and  c.concept_path like concat(i.C_FULLNAME,'%')
  and i.c_fullname not like '\Public Studies\%'
 group by i.c_fullname;
 
 
-execute immediate('truncate table biomart_user.concept_stats');
+execute immediate('truncate table BIOMART.concept_stats');
 
-insert into biomart_user.concept_stats
-SELECT SOURCESYSTEM_CD, count(1) as COUNT_OF_CONCEPTS FROM CONCEPT_DIMENSION  where concept_path not like '\\Public Studies\\%' group by SOURCESYSTEM_CD;
-
-
-execute immediate('truncate table biomart_user.observation_stats');
-
-insert into biomart_user.observation_stats
-SELECT SOURCESYSTEM_CD, count(1) as nb_observation_fact, count(distinct patient_num) as distinct_patient_num, count(distinct CONCEPT_CD) as distint_CONCEPT_CD, min(START_DATE) as min_start_date, max(START_DATE) as max_start_date FROM OBSERVATION_FACT where SOURCESYSTEM_CD not like  'EGP%' and SOURCESYSTEM_CD not like  'GSE%' group by SOURCESYSTEM_CD;
+insert into BIOMART.concept_stats
+SELECT SOURCESYSTEM_CD, count(1) as COUNT_OF_CONCEPTS FROM i2b2demodata.CONCEPT_DIMENSION  where concept_path not like '\\Public Studies\\%' group by SOURCESYSTEM_CD;
 
 
-execute immediate('truncate table "BIOMART_USER"."SCHEMA_STATS" ');
+execute immediate('truncate table BIOMART.observation_stats');
 
-insert into BIOMART_USER.SCHEMA_STATS 
+insert into BIOMART.observation_stats
+SELECT SOURCESYSTEM_CD, count(1) as nb_observation_fact, count(distinct patient_num) as distinct_patient_num, count(distinct CONCEPT_CD) as distint_CONCEPT_CD, min(START_DATE) as min_start_date, max(START_DATE) as max_start_date FROM i2b2demodata.OBSERVATION_FACT where SOURCESYSTEM_CD not like  'EGP%' and SOURCESYSTEM_CD not like  'GSE%' group by SOURCESYSTEM_CD;
+
+
+execute immediate('truncate table "BIOMART"."SCHEMA_STATS" ');
+
+insert into BIOMART.SCHEMA_STATS 
 SELECT
    owner, to_char(sum(bytes)/1024/1024/1024, '9999.999') as GB
 FROM
@@ -53,11 +53,6 @@ WHERE owner in ('I2B2METADATA', 'I2B2DEMODATA')
 GROUP BY  owner
 HAVING SUM(bytes)/1024/1024 > 10 ;-- /* Ignore really small tables */;
 commit;
-
-
-
-
-
 
   NULL;
 END COMPUTE_STATISTICS;
