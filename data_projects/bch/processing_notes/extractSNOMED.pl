2d88b35cfec93581dfc4b7057a2851d379b35263
mtmcduffie <mcduffie.michael@gmail.com>
2015-03-19 11:06:39 -0400

    AVL-145 - Updates to cTAKES processing code.

--- data_projects/bch/processing_notes/extractSNOMED.pl
+++ data_projects/bch/processing_notes/extractSNOMED.pl
@@ -5,20 +5,44 @@ use warnings;
 use Data::Dumper;
 use Benchmark qw( cmpthese );
 
-my $srcFile = "./load_nlp_data/emerge_asd_15k.txt.pre_pms_dn";
-
-print("DEBUG - parseNLP.pl : Attemping to open input file directory $srcFile\n");
+my $srcFile 			= $ARGV[0];
+my $srcSemanticFilterFile 	= $ARGV[1];
+my %validCodeHash;
 my $f;
+my $debugCounter = 0;
+
+print("DEBUG : Attemping to open input file directory $srcFile\n");
+
+open(my $semanticFilterFile, "<", $srcSemanticFilterFile) or die $!;
+
+while(my $line = <$semanticFilterFile>) {
+chomp $line;
+        my @values = split/\|/,$line,2;
+	$validCodeHash{$values[1]}++;
+}
+
+close($semanticFilterFile);
+
+print("DEBUG : Finished gathering semantic type codes\n");
+
 open($f, "<", $srcFile) or die $!;
 open my $srcFileOut, '>' , "$srcFile.out.snowmed.positive";
 
 while(my $line = <$f>) {
 	my @values = split/\|/,$line,10;
-	
+
 	if($values[3] =~ /SNO\:/ && $values[7]=='1')
 	{
-		print $srcFileOut $line;
+		my $bareSNO = $values[3];
+		$bareSNO =~ s/SNO\://;
+
+		if(exists $validCodeHash{$bareSNO})
+		{
+			print $srcFileOut $line;
+		}
 	}
+	#$debugCounter++;
+	#last if($debugCounter > 100000);
 	
 }
 
