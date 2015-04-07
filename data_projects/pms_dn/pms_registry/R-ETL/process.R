d59ed0f2418b0ca51c2d1d9ce0a5fb55c267e962
Maxime Wack <maximewack@free.Fr>
2015-04-07 14:29:39 -0400

    AVL-175 Added rules for checkbox options. Code cleaning and reformatting

--- data_projects/pms_dn/pms_registry/R-ETL/process.R
+++ data_projects/pms_dn/pms_registry/R-ETL/process.R
@@ -9,9 +9,10 @@ processFile<-function(questionnaire)
   
   # Read the data and premapping files
   data<-read.csv.2header(paste0("data",questionnaire,".csv"))
-  data<-data[!is.na(data$Survey.Session.ID),]
+  data<-data[data$Survey.Session.ID!="",]
   
-  premap<-read.csv(paste0("premap",questionnaire,".csv"),stringsAsFactors=F)
+  premap<-read.csv(paste0("premap",questionnaire,".csv"),stringsAsFactors=F,colClasses="character")
+  premap$ColNum<-as.integer(premap$ColNum)
   
   # Process each SubFile level (excluding the empty SubFile level->Demographics)
   for (subfile in levels(factor(premap$SubFile,exclude="")))
@@ -42,29 +43,7 @@ processSubfile<-function(questionnaire,subfile,data,premap)
   }
   
   # Parse resulting var names and write mappings
-  addMapping(paste0(questionnaire,"-",subfile,".txt"),ontology,1,"SUBJ_ID")
-  varNum<-2
-  ontoLevel<-0
-  for (varName in names(data2[-1]))
-  {
-    while(grepl("_",varName))
-    {
-      ontoLevel<-ontoLevel+1
-      ontology<<-push(ontology,sub("_.*$","",varName))
-      
-      varName<-sub("^.*?_","",varName)
-    }
-    
-    addMapping(paste0(questionnaire,"-",subfile,".txt"),ontology,varNum,varName)
-    
-    while(ontoLevel>0)
-    {
-      ontoLevel<-ontoLevel-1
-      ontology<<-pop(ontology)
-    }
-    
-    varNum<-varNum+1
-  }
+  addMappings(questionnaire,subfile,ontology,data2)
   
   # Write the $SubFile.txt
   write.table(data2,file=paste0("output/",questionnaire,"-",subfile,".txt"),row.names=F,sep="\t",quote=F,na="")
@@ -79,6 +58,41 @@ processHead1<-function(head1,data,premap)
   premap<-filter(premap,Head1==head1)
   
   # Anchor-based filtering of variables from the data file
+  data<-anchorFilter(premap,data)
+  
+  # Sort by Survey Date
+  data$Survey.Date<-as.numeric(strptime(data$Survey.Date,format="%Y-%m-%d %H:%M:%S"))
+  data<-arrange(data,Patient.ID,Survey.Date)
+  
+  # Delete records made less than 24 hours before the next
+  data<-filter(data,(lead(Survey.Date)-Survey.Date)>24*3600 | lead(Patient.ID)!=Patient.ID | Patient.ID==max(Patient.ID))
+  
+  # Filter for the last line of data for each patient
+  # TODO : take account of Evo to modify behavior
+  data<-data %>%
+    group_by(Patient.ID) %>%
+    filter(Survey.Date==last(Survey.Date))
+  data<-ungroup(data)
+  
+  # Reformatting needed
+  if (any(premap$Reformat!=""))
+    if (any(premap$Reformat=="1"))
+      data<-reformat(data,premap)
+    
+  # Manage 'Other Value' columns
+  if (any(grepl("_Other.Value$",names(data))))
+    data<-otherValue(data)
+  
+  # Manage "checkbox" items
+  if (any(grepl("_Unsure$",names(data))))
+    data<-checkboxes(data)
+  
+  select(data,-Survey.Date,-Birthdate)
+}
+
+# Anchor-based filtering of data from the mapping file
+anchorFilter<-function(premap,data)
+{
   # Find the index of the first variable by name in the data file
   idx1<-premap$ColNum[1] #premap's index of the variable
   idx2<-which(names(data)==premap$Header[1]) #data's index of the variable
@@ -90,104 +104,116 @@ processHead1<-function(head1,data,premap)
   }
   # If real homonyms at first position of head1, use an heuristic (pick the nearest one in position)
   if (length(idx2)>1)
-  {
     idx2<-idx2[order(abs(idx2-idx1))[1]]
-  }
-  
+
   # Update indexes in the premap
   premap<-mutate(premap,ColNum=ColNum+(idx2-idx1))
+
   # Filter columns in data based on the updated index
-  data<-data[c(1:3,premap$ColNum)]
-  
-  # Sort by Survey Date
-  data$Survey.Date<-as.numeric(strptime(data$Survey.Date,format="%Y-%m-%d %H:%M:%S"))
-  data<-arrange(data,Patient.ID,Survey.Date)
-  
-  # Delete records made less than 24 hours before the next
-  data<-filter(data,(lead(Survey.Date)-Survey.Date)>24*3600 | lead(Patient.ID)!=Patient.ID | Patient.ID==max(Patient.ID))
-  data<-select(data,-Survey.Date)
-  
-  # Filter for the last line of data for each patient
-  # TODO : take account of HistEvo to modify behavior
-  data<-data %>%
-    group_by(Patient.ID) %>%
-    filter(row_number(desc(Survey.Session.ID))==1)
-  
+  data[c(which(names(data) %in% c("Patient.ID","Survey.Date","Birthdate")),premap$ColNum)]
+}
+
+reformat<-function(data,premap)
+{
   # Create new data frame to contain transformed/curated data
-  data2<-select(data,Patient.ID)
+  data2<-select(data,Patient.ID,Survey.Date,Birthdate)
   
-  # Reformatting needed
-  if (any(premap$Reformat==1))
+  # new vars prefix
+  varPre<-levels(factor(unlist(data[premap$Header[premap$Reformat=="1"]]),exclude=""))
+  # new vars suffix
+  varSuff<-levels(factor(premap$VarName[premap$Linked!=""]))
+  # Create new vars after transform
+  for (pre in varPre)
+    for (suff in varSuff)
+      data2[[paste(pre,suff,sep="_")]]<-NA
+  
+  #Effectively reformat the variable, keeping linked variables together
+  for (patient in data2$Patient.ID)
   {
-    # new vars prefix
-    varPre<-levels(factor(unlist(data[premap$Header[premap$Reformat==1]]),exclude=""))
-    # new vars suffix
-    varSuff<-levels(factor(premap$VarName[!is.na(premap$Linked)]))
-    # Create new vars after transform
-    for (pre in varPre)
+    for (link in unique(premap$Linked))
     {
+      pre<-data[data$Patient.ID==patient,premap$Header[premap$Linked==link & premap$Reformat=="1"]]
       for (suff in varSuff)
       {
-        data2[[paste(pre,suff,sep="_")]]<-NA
-      }
-    }
-    
-    #Effectively reformat the variable, keeping linked variables together
-    for (patient in data2$Patient.ID)
-    {
-      for (link in levels(factor(premap$Linked)))
-      {
-        pre<-data[data$Patient.ID==patient,premap$Header[premap$Linked==link & premap$Reformat=="1"]]
-        for (post in levels(factor(premap[premap$Linked==link,]$VarName)))
+        if (premap$Reformat[premap$Linked==link & premap$VarName==suff]=="1")
         {
-          if (premap$Reformat[premap$Linked==link & premap$VarName==post])
-          {
-            data2[data2$Patient.ID==patient,][[paste(pre,post,sep="_")]]<-"Yes"
-          }
-          else
-          {
-            data2[data2$Patient.ID==patient,][[paste(pre,post,sep="_")]]<-data[data$Patient.ID==patient,premap$Header[premap$Linked==link & premap$VarName==post]]
-          }
+          data2[data2$Patient.ID==patient,][[paste(pre,suff,sep="_")]]<-"Yes"
+        }
+        else
+        {
+          data2[data2$Patient.ID==patient,][[paste(pre,suff,sep="_")]]<-data[data$Patient.ID==patient,premap$Header[premap$Linked==link & premap$VarName==suff]]
         }
       }
     }
-    
-    for (varName in names(data2))
-    {
-      data2[[varName]]<-unlist(data2[[varName]])
-    }
-    
-    varnames<-names(data2)
-    varnames[-1]<-paste(head1,varnames[-1],sep="_")
-    varnames<-gsub("^_","",varnames)
-    
-    colnames(data2)<-varnames
-  }
-  else # Reformatting not needed
-  {
-    data2<-select(data,-Survey.Session.ID)
   }
   
-  data2<-ungroup(data2)
+  # Rename variables
+  for (varName in names(data2))
+    data2[[varName]]<-unlist(data2[[varName]])
   
-  # Manage 'Other Value' columns
-  if (length(select(data2,ends_with("_Other.Value"))) != 0)
-  {
-    # When there is an 'Other' column
-    if (length(select(data2,ends_with("_Other"))) != 0)
-    {
-      varNameOther<-names(select(data2,ends_with("_Other")))
-      varNameOtherValue<-names(select(data2,ends_with("_Other.Value")))
-      data2[[varNameOther]][!is.na(data2[[varNameOther]])]<-data2[[varNameOtherValue]][!is.na(data2[[varNameOther]])]
-    } # When there are only two columns (+1 for Patient.ID)
-    else if (length(data2)==3)
-    {
-      varNameOther<-names(select(data2,-Patient.ID,-ends_with("_Other.Value")))
-      varNameOtherValue<-names(select(data2,ends_with("_Other.Value")))
-      data2[[varNameOther]][data2[[varNameOther]]=="Other"]<-data2[[varNameOtherValue]][data2[[varNameOther]]=="Other"]
-    }
-    data2<-select(data2,-ends_with("_Other.Value"))
-  }
+  varnames<-names(data2)
+  varnames[-(1:3)]<-paste(premap$Head1[1],varnames[-(1:3)],sep="_")
+  varnames<-gsub("^_","",varnames)
+  
+  colnames(data2)<-varnames
   
   data2
+}
+
+otherValue<-function(data)
+{
+  colOtherValue<-grep("_Other.Value$",names(data))
+  # When there is an 'Other' column
+  if (any(grepl("_Other$",names(data))))
+  {
+    colOther<-grep("_Other$",names(data))
+    data[data[colOther]=="1",colOther]<-data[data[colOther]=="1",colOtherValue]
+  } # When there are only two columns (+3 for Patient.ID,Survey.Date,Birthdate)
+  else if (length(data)==5)
+  {
+    colOther<-grep("_Other.Value",names(data[-(1:3)]),invert=T)+3
+    data[data[colOther]=="Other",colOther]<-data[data[colOther]=="Other",colOtherValue]
+  }
+  
+  select(data,-ends_with("_Other.Value"))
+}
+
+checkboxes<-function(data)
+{
+  ## Set "_Other" column aside
+  if (any(grepl("_Other$",names(data))))
+  {
+    colOther<-grep("_Other$",names(data))
+    varOther<-data[[colOther]]
+    data[varOther!="",colOther]<-"1"
+  }
+  
+  ## Create helping columns
+  colSpe<-grep("_(Unsure|Not applicable|No(ne.*| intervention)?)",names(data))
+  colData<-grep("_(Unsure|Not applicable|No(ne.*| intervention)?)",names(data[-(1:3)]),invert=T)+3
+  sumSpe<-apply(data[colSpe],1,function(x){sum(as.integer(x),na.rm=T)})
+  sumData<-apply(data[colData],1,function(x){sum(as.integer(x),na.rm=T)})
+  
+  ## Replace noise with empty data
+  # Multiple special (No/Unsure/Not applicable) columns checked at the same time
+  data[sumSpe>1,colData]<-""
+  # Data column(s) and special column(s) checked at the same time
+  data[sumData>0 & sumSpe>0,colData]<-""
+    
+  ## Only one special column checked
+  for (col in colSpe)
+    data[data[col]=="1",col]<-gsub("\\."," ",sub(".*_([^_]+)$","\\1",names(data[col])))
+  
+  data[sumSpe==1 & sumData==0,colData]<-apply(data[colSpe],1,paste,collapse="")[sumSpe==1 & sumData==0]
+  
+  ## Normal case
+  for (col in colData)
+      data[sumData>0 & sumSpe==0,col]<-ifelse(data[sumData>0 & sumSpe==0,col]=="1","Yes","No")
+    
+  ## Re-fill "Other" columns with its values
+  if (any(grepl("_Other$",names(data))))
+    data[data[colOther]=="Yes",colOther]<-varOther[data[colOther]=="Yes"]
+  
+  ## Remove special columns
+  select(data,-matches("_(Unsure|Not applicable|No(ne.*| intervention)?)"))
 }
