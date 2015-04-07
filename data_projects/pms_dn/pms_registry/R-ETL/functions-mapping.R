d5659ac199973d35ba1549f81c48ae7778ddb8a7
Maxime Wack <maximewack@free.Fr>
2015-04-07 16:58:14 -0400

    AVL-185 Comment cleaning

--- data_projects/pms_dn/pms_registry/R-ETL/functions-mapping.R
+++ data_projects/pms_dn/pms_registry/R-ETL/functions-mapping.R
@@ -26,24 +26,7 @@ addMappings<-function(questionnaire,subfile,ontology,data)
   }
 }
 
-#' Add a line to the mapping file
-#' 
-#' Add one new line to the mapping file object
-#' 
-#' Add one new line to the mapping file object by giving the data source file name, the column number,
-#' the data label, and a list of levels for the ontology.
-#' The levels for the ontology will have spaces replaced by underscores (_), and be concatenated with
-#' a separating plus sign (+).
-#' @encoding UTF-8
-#' @param path Path to which create the mapping file
-#' @param dataFile The data source file name
-#' @param columnNum The column number from the data source file
-#' @param dataLabel The desired label in the ontology
-#' @param categoryCode A vector of ontology levels in the correct order
-#' @return An updated mapping file object
-#' @examples
-#' \dontrun{mapping <- addMapping("data.txt",1,SUBJ_ID,"New ontology","Demographics")}
-#' @export
+# Add a line to the mapping file
 addMapping <- function(dataFile,categoryCode,columnNum,dataLabel)
 {
   categoryCode <- paste(categoryCode,collapse="+")
@@ -56,14 +39,14 @@ addMapping <- function(dataFile,categoryCode,columnNum,dataLabel)
   write.table(mapping,file="output/mapping.txt",row.names=F,sep="\t",append = T,col.names=F,quote=F)
 }
 
-#' Simple push mechanism
+# Simple push mechanism
 push<-function(vector,value)
 {
   vector<-c(vector,value)
   vector
 }
 
-#' Simple pop mechanism. Does not return the popped value but the modified vector
+# Simple pop mechanism. Does not return the popped value but the modified vector
 pop<-function(vector)
 {
   if (length(vector)==1)
