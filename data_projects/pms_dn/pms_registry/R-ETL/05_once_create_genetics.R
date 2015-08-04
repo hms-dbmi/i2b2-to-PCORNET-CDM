f6bec5e39ded8d7afd50eee64beb4a0ef6394bc8
Maxime Wack <maximewack@free.Fr>
2015-08-04 17:46:08 -0400

    AVL-185 Reorganizing files. Fifth (do once) step

---
+++ data_projects/pms_dn/pms_registry/R-ETL/05_once_create_genetics.R
@@ -0,0 +1,16 @@
+source("functions-process.R")
+
+#==== Genetic information ====
+#Extract the information to an external file
+clinical <- read.csv.2header("dataClinical.csv")
+clinical[c(1,5,19:67)] %>%
+  mutate(Test.Date = gsub("/", "-",                      Test.Date, perl = T)) %>%
+  mutate(Test.Date = gsub("^(\\d)-", "0\\1-",            Test.Date, perl = T)) %>%
+  mutate(Test.Date = gsub("-(\\d)-", "-0\\1-",           Test.Date, perl = T)) %>%
+  mutate(Test.Date = gsub("-(\\d{2})$", "-20\\1",        Test.Date, perl = T)) %>%
+  mutate(Test.Date = gsub("(^\\d{2}-\\d{4}$)", "01-\\1", Test.Date, perl = T)) %>%
+  mutate(Test.Date = gsub("^[\\w ]+$", "",               Test.Date, perl = T)) %>%
+  mutate(Test.Date = as.Date(Test.Date, format = "%m-%d-%Y")) %>%
+  arrange(Patient.ID, Test.Date) %>%
+  distinct -> Genetics
+write.csv(Genetics, "Genetics.csv")
